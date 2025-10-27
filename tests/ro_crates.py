0864770cc195eb0bc62f9c9f625c671be04beeff
Marco Enrico Piras <kikkomep@crs4.it>
2025-10-27 11:13:53 +0000

    fix(model): :ambulance: fix availability check of external resources

--- tests/ro_crates.py
+++ tests/ro_crates.py
@@ -57,7 +57,10 @@ class ValidROC:
 
     @property
     def bagit_remote_zip(self) -> str:
-        return "https://github.com/kikkomep/rocrate-validator/raw/refs/heads/feat/configurable-root-issue-100/tests/data/crates/valid/bagit.zip"
+        return (
+            "https://github.com/kikkomep/rocrate-validator/raw/refs/heads/"
+            "feat/configurable-root-issue-100/tests/data/crates/valid/bagit.zip"
+        )
 
     @property
     def workflow_roc(self) -> Path:
