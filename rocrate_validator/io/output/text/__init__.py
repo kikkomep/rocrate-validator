4ce1db2af6a0238571ecafd2d25d659af049ce25
Marco Enrico Piras <kikkomep@crs4.it>
2025-11-19 16:57:01 +0000

    refactor(io.output): :recycle: rename formatting classes

--- rocrate_validator/io/output/text/__init__.py
+++ rocrate_validator/io/output/text/__init__.py
@@ -23,8 +23,8 @@ import rocrate_validator.log as logging
 from rocrate_validator.models import ValidationResult, ValidationStatistics
 
 from .. import BaseOutputFormatter
-from .formatters import (ValidationResultTextOutputFormatterNew,
-                         ValidationStatisticsTextOutputFormatterNew)
+from .formatters import (ValidationResultTextOutputFormatter,
+                         ValidationStatisticsTextOutputFormatter)
 
 # set up logging
 logger = logging.getLogger(__name__)
@@ -34,8 +34,8 @@ class TextOutputFormatter(BaseOutputFormatter):
 
     def __init__(self, data: Optional[Any] = None):
         super().__init__(data)
-        self.add_type_formatter(ValidationResult, ValidationResultTextOutputFormatterNew)
-        self.add_type_formatter(ValidationStatistics, ValidationStatisticsTextOutputFormatterNew)
+        self.add_type_formatter(ValidationResult, ValidationResultTextOutputFormatter)
+        self.add_type_formatter(ValidationStatistics, ValidationStatisticsTextOutputFormatter)
 
     def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
         yield from super().__rich_console__(console, options)
