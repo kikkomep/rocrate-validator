9141a727d4f4e1d405cd998981a27fcb48f45e3a
Marco Enrico Piras <kikkomep@crs4.it>
2026-01-09 14:25:19 +0000

    fix(core): :bug: enable skipping of SHACL checks

--- rocrate_validator/requirements/shacl/checks.py
+++ rocrate_validator/requirements/shacl/checks.py
@@ -200,11 +200,12 @@ class SHACLCheck(RequirementCheck):
             assert shape is not None, "Unable to map the violation to a shape"
             requirementCheck = SHACLCheck.get_instance(shape)
             assert requirementCheck is not None, "The requirement check cannot be None"
-            failed_requirements_checks.add(requirementCheck)
-            violations = failed_requirements_checks_violations.get(requirementCheck.identifier, None)
-            if violations is None:
-                failed_requirements_checks_violations[requirementCheck.identifier] = violations = []
-            violations.append(violation)
+            if requirementCheck.identifier not in shacl_context.settings.skip_checks:
+                failed_requirements_checks.add(requirementCheck)
+                violations = failed_requirements_checks_violations.get(requirementCheck.identifier, None)
+                if violations is None:
+                    failed_requirements_checks_violations[requirementCheck.identifier] = violations = []
+                violations.append(violation)
         # sort the failed checks by identifier and severity
         # to ensure a consistent order of the issues
         # and to make the fail fast mode deterministic
